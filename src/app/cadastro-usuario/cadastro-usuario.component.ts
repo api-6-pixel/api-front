0c632aed5c2ad09c8b8a3932a4a49d32dd08360c
alisson-C-angular-php <aissonsantos928@gmail.com>
2025-04-21 00:55:40 -0300

    feat/PX-90: Integracao com backend de cadastro de usuario

--- src/app/cadastro-usuario/cadastro-usuario.component.ts
+++ src/app/cadastro-usuario/cadastro-usuario.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { NgFor, NgIf } from '@angular/common';
 import { ScrollbarDirective } from '../scrollbar.directive';
 import { ModalTermoUsuarioComponent } from '../modal-termo-usuario/modal-termo-usuario.component';
+import { HttpService } from '../service/http.service';
 
 @Component({
   selector: 'app-cadastro-usuario',
@@ -24,14 +25,51 @@ export class CadastroUsuarioComponent implements OnInit {
   cpf:string="";
   email:string="";
   usuarioNome:string="";
-  constructor(private modalCtrl: ModalController) {}
+  constructor(private modalCtrl: ModalController,private http:HttpService) {}
 
   ngOnInit() {}
 
+  
   async openModal() {
     const modal = await this.modalCtrl.create({
       component: ModalTermoUsuarioComponent,
     });
-    return await modal.present();
+  
+    await modal.present();
+  
+    const { data } = await modal.onDidDismiss();
+  
+    if (data) {
+  
+      if (data.accepted === false) {
+        localStorage.setItem("termo","recusou")
+        console.log('Usuário recusou os termos');
+      } else if (data.accepted === true) {
+        console.log('Usuário aceitou os termos');
+      } else {
+        console.log('Modal fechado sem ação definida');
+      }
+    }
+  }
+  
+
+
+  enviarDados(){
+    var termo = localStorage.getItem("termo");
+    if(termo == "recusou"){
+      return;
+    }
+    const body = {
+      nome:this.usuarioNome,
+      email:this.email,
+      senha:this.senha,
+      documento:this.cpf
+    }
+
+    this.http.post("usuarios", body)
+      .then((m) => console.log("ok"))
+        .catch((error: any) => console.log(error)) 
+
+  
   }
 }
--- src/app/modal-termo-usuario/modal-termo-usuario.component.ts
+++ src/app/modal-termo-usuario/modal-termo-usuario.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicModule, ModalController } from '@ionic/angular';
+import { IonicModule, ModalController, ToastController } from '@ionic/angular';
 import { NgIf, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ScrollbarDirective } from '../scrollbar.directive';
+import { HttpService } from '../service/http.service';
+import { body } from 'ionicons/icons';
 
 @Component({
   selector: 'app-modal-termo-usuario',
@@ -18,47 +20,113 @@ import { ScrollbarDirective } from '../scrollbar.directive';
   ],
 })
 export class ModalTermoUsuarioComponent implements OnInit {
-  termoObrigatorio1 = false;
-  termoObrigatorio2 = false;
-  termoOpcional1 = false;
-  termoOpcional2 = false;
+  termoId: number = 0;
+  descricao: string = '';
+  itensObrigatorios: any[] = [];
+  itensOpcionais: any[] = [];
+  aceito:boolean = false;
+  respostas: { [codigoItem: number]: boolean } = {};
 
-  constructor(private modalCtrl: ModalController) {}
+  constructor(
+    private modalCtrl: ModalController,
+    private http: HttpService,
+    public toastController: ToastController
+  ) {}
 
-  ngOnInit(): void {}
-
-  podeConcordar(): boolean {
-    return this.termoObrigatorio1 && this.termoObrigatorio2;
+  ngOnInit() {
+    this.http.get('historico/ativo?termoItemCodigo=1').then((data: any) => {
+      this.termoId = data.termoId;
+      this.descricao = data.descricao;
+      this.itensObrigatorios = data.obrigatorios;
+      this.itensOpcionais = data.opcionais;
+      this.aceito = data.aceito; 
+      if(this.aceito){
+        [...this.itensObrigatorios, ...this.itensOpcionais].forEach((item) => {
+          this.respostas[item.codigo] = true;
+        });
+      }
+      
+      else{
+      [...this.itensObrigatorios, ...this.itensOpcionais].forEach((item) => {
+        this.respostas[item.codigo] = false;
+      });
+    }});
   }
 
+  podeConcordar(): boolean {
+    // Verifica se todos os obrigatórios foram aceitos
+    return this.itensObrigatorios.every(
+      (item) => this.respostas[item.codigo]
+    );
+  }
+
+  async exibirToast(mensagem: string, cor: string) {
+    const toast = await this.toastController.create({
+      message: mensagem,
+      duration: 3000,
+      position: 'bottom',
+      color: cor,
+    });
+    await toast.present();
+  }
 
   concordar() {
     const termosAceitos = {
-      obrigatorios: {
-        termo1: this.termoObrigatorio1,
-        termo2: this.termoObrigatorio2,
-      },
-      opcionais: {
-        termo1: this.termoOpcional1,
-        termo2: this.termoOpcional2,
-      },
-      consentidoEm: new Date().toISOString(),
+      usuarioCodigo: 1,  // Substitua por código do usuário, se necessário
+      termoItemCodigo: this.termoId,
+      respostas: this.respostas,  // Envia todas as respostas (true/false)
     };
 
-    localStorage.setItem("termos",JSON.stringify(termosAceitos))
-  
+    localStorage.setItem('termos', JSON.stringify(termosAceitos));
+
+    // Envia os dados para o backend
+    this.http.post("historico/aceite", termosAceitos).then((response: any) => {
+      this.exibirToast("Termos aceitos!", "success");
+    }).catch((error: any) => {
+      this.exibirToast("Erro ao aceitar os termos!", "danger");
+    });
+
     this.modalCtrl.dismiss({
       accepted: true,
       termos: termosAceitos,
     });
   }
+
+  
+  recusar() {
+    // Define explicitamente todos os valores como false
+    const respostasRecusadas: { [codigo: number]: boolean } = { ...this.respostas };
+  
+    Object.keys(respostasRecusadas).forEach(codigo => {
+      // Converte a chave de string para número, pois `Object.keys` retorna um array de strings
+      const codigoNumero = Number(codigo);
+      respostasRecusadas[codigoNumero] = false; // Marca todos como não aceitos
+    });
+  
+    const termosAceitos = {
+      usuarioCodigo: 1,
+      termoItemCodigo: this.termoId,
+      respostas: respostasRecusadas,
+    };
+  
+    localStorage.setItem('termos', JSON.stringify(termosAceitos));
+  
+    // Envia os dados para o backend
+    this.http.post("historico/aceite", termosAceitos).then((response: any) => {
+      this.exibirToast("Termos recusados!", "success");
+    }).catch((error: any) => {
+      this.exibirToast("Erro ao recusar os termos!", "danger");
+    });
   
 
-  recusar() {
+    
     this.modalCtrl.dismiss({
       accepted: false,
+      termos: termosAceitos,
     });
   }
+  
+  
 
   close() {
     this.modalCtrl.dismiss();
